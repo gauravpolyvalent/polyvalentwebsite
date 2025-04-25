1b38457bcc7b116cd590f48414b73bddd815f401
crm.whytefarms <gauravpolyvalent>
2025-04-25 11:59:08 +0530

    add new website

---
+++ assets/js/app.js
@@ -0,0 +1,31 @@
+ const burger = document.querySelector(".burger-menu");
+ const nav = document.querySelector(".navbar-header");
+burger.addEventListener("click", () => {
+        nav.classList.toggle("active");
+    });
+
+// cursor code here 
+
+$(document).ready(function () {
+    $('.client-slider').slick({
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        arrows: true,
+        dots: false,
+        autoplay: true,
+        autoplaySpeed: 3000,
+        prevArrow: '<button type="button" class="slick-prev"><i class="fas fa-arrow-left"></i></button>',
+        nextArrow: '<button type="button" class="slick-next"><i class="fas fa-arrow-right"></i></button>',
+        responsive: [
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 1,
+                    arrows: false,
+                }
+            }
+        ]
+    });
+});
+
+
